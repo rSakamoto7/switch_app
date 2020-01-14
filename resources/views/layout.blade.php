1a8d048981fda53fd4167e8a96be50072ca2522b
ユーザー名Ryota <rsakamoto0702@gmail.com>
2020-01-15 02:59:26 +0900

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,7 @@
     <body>
         <header>
             <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">ああメカニカルスイッチアドバイザー</a>
+                <a class="navbar-brand" href="/">uメカニカルスイッチアドバイザー</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" area-expanded="false" area-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
